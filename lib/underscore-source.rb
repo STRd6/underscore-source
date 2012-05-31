b4b87f5645cfdd253c4573aea1cb91eef168fd7d
Daniel X. Moore <yahivin@gmail.com>
2012-05-31 16:17:59 -0700

    Set up gem

---
+++ lib/underscore-source.rb
@@ -0,0 +1,7 @@
+require "underscore-source/version"
+
+module Underscore
+  module Source
+    # Your code goes here...
+  end
+end
---
+++ lib/underscore-source/version.rb
@@ -0,0 +1,5 @@
+module Underscore
+  module Source
+    VERSION = "0.0.1"
+  end
+end
